6069d133457b31dbcf38ba16f91107479c557155
Kyeongyunhui <yunhui72659147@gmail.com>
2025-03-13 13:42:59 +0900

    ff

--- script/script.js
+++ script/script.js
@@ -142,3 +142,34 @@ track.addEventListener('transitionend', () => {
         track.style.transition = 'transform 0.5s ease';
     }
 });
+
+
+
+
+
+
+
+// 탭메뉴
+const tabs = document.querySelectorAll('.tab');
+        const store = document.querySelectorAll('.store');
+        console.log(tabs);
+        console.log(store);
+
+        tabs.forEach(tab => {
+            // forEach((item, index, array) => {
+            // item : 현재 순화중인 배열의 요소(필수!, 작명은 자유)
+            // index : 현재 요소의 인덱스(선택)
+            // array : 현재 순화중인 배열 자체(선택)
+            // });
+            store.addEventListener('click',()=>{
+                tabs.forEach(t => t.classList.remove('active'));
+                tabContent.forEach(tc => tc.classList.remove('active'));
+                store.classList.add('active');
+                document.getElementById('tab_' + store.dataset.tab).classList.add('active');
+                // dataset : HTML 요소의 data-* 속성을 읽거나 설정할 수 있는 js객체
+                // tab.dataset : 요소의 모든 data-tab 번호를 가져옴
+                // tab.dataset.tab : domString이 아닌 tab속성의 값만 빼오겠다는 뜻
+                console.log(store.dataset);
+                console.log(store.dataset.tab);
+            });
+        })
