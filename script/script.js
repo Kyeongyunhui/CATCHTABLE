d3e0558d2d3b741809b7cea47ee0cec1a01938ef
Kyeongyunhui <yunhui72659147@gmail.com>
2025-03-18 14:58:15 +0900

    day9

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 const slides = document.querySelector('.slides');
-const mainDots = document.querySelectorAll('.slideDots');
+const mainDots = document.querySelectorAll('.slideDot');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 let currentIndex = 0;
 const totalSlides = 3; // 실제 슬라이드 개수 (복사된 6번째 슬라이드는 제외)
 let interval;
 
-function updateSlide(index) {
-    slides.style.transition = 'transform 0.5s ease-in-out';
-    slides.style.transform = `translateX(-${index * 100}'vw)`;
+function updateSlide(index, smooth = true) {
+    slides.style.transition = smooth ? 'transform 0.5s ease-in-out' : 'none';
+    slides.style.transform = `translateX(-${index * 100}vw)`;
 
     // 닷 업데이트
     mainDots.forEach(slideDots => slideDots.classList.remove('active'));
@@ -21,21 +21,18 @@ function nextSlide() {
 
     if (currentIndex === totalSlides) {
         setTimeout(() => {
-            slides.style.transition = 'none';
             currentIndex = 0;
-            slides.style.transform = `translateX(0vw)`;
+            updateSlide(currentIndex, false);
         }, 500);
     }
 }
 
 function prevSlide() {
     if (currentIndex === 0) {
-        slides.style.transition = 'none';
         currentIndex = totalSlides;
-        slides.style.transform = `translateX(-${currentIndex * 100}vw)`;
+        updateSlide(currentIndex, false);
 
         setTimeout(() => {
-            slides.style.transition = 'transform 0.5s ease-in-out';
             currentIndex--;
             updateSlide(currentIndex);
         }, 10);
@@ -152,24 +149,57 @@ track.addEventListener('transitionend', () => {
 // 탭메뉴
 const tabs = document.querySelectorAll('.tab');
 const store = document.querySelectorAll('.store');
-        console.log(tabs);
-        console.log(store);
+console.log(tabs);
+console.log(store);
 
-        tabs.forEach(tab => {
-            // forEach((item, index, array) => {
-            // item : 현재 순화중인 배열의 요소(필수!, 작명은 자유)
-            // index : 현재 요소의 인덱스(선택)
-            // array : 현재 순화중인 배열 자체(선택)
-            // });
-            tab.addEventListener('click',()=>{
-                tabs.forEach(t => t.classList.remove('active'));
-                store.forEach(tc => tc.classList.remove('active'));
-                tab.classList.add('active');
-                document.getElementById('tab_' + tab.dataset.tab).classList.add('active');
-                // dataset : HTML 요소의 data-* 속성을 읽거나 설정할 수 있는 js객체
-                // tab.dataset : 요소의 모든 data-tab 번호를 가져옴
-                // tab.dataset.tab : domString이 아닌 tab속성의 값만 빼오겠다는 뜻
-                console.log(store.dataset);
-                console.log(store.dataset.tab);
-            });
-        })
+tabs.forEach(tab => {
+    // forEach((item, index, array) => {
+    // item : 현재 순화중인 배열의 요소(필수!, 작명은 자유)
+    // index : 현재 요소의 인덱스(선택)
+    // array : 현재 순화중인 배열 자체(선택)
+    // });
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        store.forEach(tc => tc.classList.remove('active'));
+        tab.classList.add('active');
+        document.getElementById('tab_' + tab.dataset.tab).classList.add('active');
+        // dataset : HTML 요소의 data-* 속성을 읽거나 설정할 수 있는 js객체
+        // tab.dataset : 요소의 모든 data-tab 번호를 가져옴
+        // tab.dataset.tab : domString이 아닌 tab속성의 값만 빼오겠다는 뜻
+        console.log(store.dataset);
+        console.log(store.dataset.tab);
+    });
+});
+
+
+// 헤더 페이드인
+
+
+const header = document.querySelector("header");
+const headerMenu = document.querySelector(".menu");
+const headerLogo = document.querySelector(".logo");
+
+let posY = 100; //스크롤 위치값 넣을 변수 생성
+window.addEventListener("scroll", (e) => {
+    if (posY < window.scrollY) {
+        headerHide();
+    } else {
+        headerShow();
+    }
+    posY = window.scrollY;
+});
+
+function headerHide() {
+    header.classList.add("hide");
+    headerMenu.style.background = "rgba(0,0,0,0)";
+    headerLogo.style.color = "#FF3D00";
+
+
+}
+
+function headerShow() {
+    header.classList.remove("hide");
+    headerLogo.style.color = "#ffffff";
+    headerMenu.style.background = "#FF3D00";
+
+}
